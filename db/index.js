6b314b68e74a2b51981a0225eb1f9b8a0292f3f7
Ian A <n01384067@unf.edu>
2020-06-28 19:03:36 -0400

    333

--- db/index.js
+++ db/index.js
@@ -1,6 +1,9 @@
 // Connect to DB
+require("dotenv").config();
+const client = new Client(
+  process.env.DATABASE_URL || "postgres://localhost:5432/linkerator"
+);
 const { Client } = require("pg");
-const client = new Client("postgres://localhost:5432/linkerator");
 // database methods
 
 async function createLink({ linkname, count, comment }) {
